9d044c25d073ed17ac8be19c6c07d28256fb0a30
axtyet <144403008+axtyet@users.noreply.github.com>
2024-04-06 08:39:36 +0800

    Update generate_image_json.py

--- .github/scripts/generate_image_json.py
+++ .github/scripts/generate_image_json.py
@@ -9,23 +9,25 @@ def generate_json():
         "description": "自用图标，不定期更新",
         "icons": []
     }
-    
-    for filename in os.listdir(image_folder):
-        if filename.endswith(".png"):
-            image_path = os.path.join(image_folder, filename)
-            raw_url = f"https://raw.githubusercontent.com/{os.environ['GITHUB_REPOSITORY']}/main/{image_path}"
-            json_data["icons"].append({"name": filename, "url": raw_url})
+    #递归遍历Icons文件夹及其子文件夹中的。png文件
+    for root, dirs, files in os.walk(image_folder):
+        for file in files:
+           if file.endswith(".png"):
+               filename = os.path.splitext(file)[0]
+               image_path = os.path.join(root, file).replace('\\', '/')  #保证路径在所有操作系统上的一致性
+               raw_url = f"https://raw.githubusercontent.com/{os.environ['GITHUB_REPOSITORY']}/main/{image_path}"
+               json_data["icons"].append({"name": filename, "url": raw_url})
 
     # 按照name键对列表中的字典进行排序
     json_data["icons"].sort(key=lambda item:item['name'])
 
-    # Set the output path relative to the repository root
+    # 将json数据写入icons.json文件
     output_path = os.path.join(os.getcwd(), 'icons.json')
 
     with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(json_data, json_file, ensure_ascii=False, indent=2)
 
-    # Save output data to the GITHUB_STATE environment file
+    # 更新环境文件
     with open(os.environ['GITHUB_STATE'], 'a') as state_file:
         state_file.write(f"ICONS_JSON_PATH={output_path}\n")
 
